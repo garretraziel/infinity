df18b5682fb8ab35d9199b28384a7186ab1016c4
Garret Raziel <boloomka@gmail.com>
2013-07-17 12:59:18 +0200

    arguments parsing

--- infinity.py
+++ infinity.py
@@ -1,10 +1,25 @@
 #!/usr/bin/env python
 
-def load_config():
+import argparse
+
+
+def load_config(path):
     pass
 
-def main():
+
+def run(path, config):
     pass
+
+
+def main():
+    parser = argparse.ArgumentParser(description="Program for running automatic GUI tests.")
+    parser.add_argument("-c", "--config", default="/etc/infinity.cfg",
+                        help="path to infinity config file (default: %(default)s)")
+    parser.add_argument("directory", default=".", nargs="?", help="directory with tests to run (default: %(default)s)")
+    args = parser.parse_args()
+    config = load_config(args.config)
+    run(args.directory, config)
+
 
 if __name__ == "__main__":
     main()
