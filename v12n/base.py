12afb7561af780afc96fe5c1987781fc72ffa15c
Garret Raziel <boloomka@gmail.com>
2013-07-23 14:52:28 +0200

    more v12n modifications

--- v12n/base.py
+++ v12n/base.py
@@ -12,71 +12,11 @@ import os
 import libvirt
 import inflogging
 from xml.dom.minidom import parseString
+from inftest import InfinityException
 
 LIBVIRT_CONNECTION = None
 MANAGED_VM_DOMAINS = []
-POOL_NAME = None
-
-
-def setup_v12n(uri, pool_path):
-    global LIBVIRT_CONNECTION, POOL_NAME
-
-    # connect to libvirt
-    if not LIBVIRT_CONNECTION:
-        LIBVIRT_CONNECTION = libvirt.open(uri)  # needs root access
-
-    # create pool for storages
-    if not POOL_NAME:
-        return
-
-    xml_file = open("xml/pool.xml")
-    xml_pool = xml_file.read()
-    xml_pool = xml_pool.format(path=pool_path)
-    if not os.path.exists(pool_path):
-        os.mkdir(pool_path)
-    xml_dom = parseString(xml_pool)
-    POOL_NAME = xml_dom.getElementsByTagName("name")
-    if len(POOL_NAME) != 1:
-        inflogging.log("Bad pool XML.", "ERROR")
-    POOL_NAME = POOL_NAME[0].value
-    if POOL_NAME not in LIBVIRT_CONNECTION.listAllStoragePools(0):
-        LIBVIRT_CONNECTION.storagePoolCreateXML(xml_pool, 0)
-
-
-def clean_all():
-    """Clean all existing domains, storages and pool."""
-
-    global LIBVIRT_CONNECTION
-    if not LIBVIRT_CONNECTION:
-        return
-
-
-
-
-def clean_domain():
-    """Clean existing domain, storage or volume."""
-
-    global LIBVIRT_CONNECTION
-    if not LIBVIRT_CONNECTION:
-        return
-
-    domains = LIBVIRT_CONNECTION.listAllDomains(0)
-    domains = [(domain.name(), domain) for domain in domains]
-
-    if "alfa-infinity" in domains:
-        dom = conn.lookupByName("alfa-infinity")
-        dom.destroy()
-
-    pools = conn.listAllStoragePools(0)
-    pools = [pool.name() for pool in pools]
-    if "infinity-disks" in pools:
-        pool = conn.storagePoolLookupByName("infinity-disks")
-
-        vols = pool.listVolumes()
-        if "disk0.qcow2" in vols:
-            vol = pool.storageVolLookupByName("disk0.qcow2")
-            vol.delete(0)
-        pool.destroy()
+MANAGED_POOL = None
 
 
 class VirtualMachine(object):
@@ -101,25 +41,57 @@ class VirtualMachine(object):
         self.pool = pool
 
     def delete(self):
-        """Destroy VirtualMachine object, deleting associated domain, storage and pool."""
+        """Destroy VirtualMachine object, deleting associated domain and storage."""
         self.domain.destroy()
         self.storage.delete(0)
-        self.pool.destroy()
+        #self.pool.destroy()
 
 
-def create_full_path(path):
-    """Return full path to given path.
+def setup_v12n(uri, pool_path):
+    global LIBVIRT_CONNECTION, MANAGED_POOL
 
-    If path is relative, return RESOURCES_PATH + path, otherwise return path.
-    """
-    #TODO: RESOURCES_PATH should be configured for each infinity structure independently
-    if os.path.isabs(path):
-        fullpath = path
+    # connect to libvirt
+    if not LIBVIRT_CONNECTION:
+        LIBVIRT_CONNECTION = libvirt.open(uri)  # needs root access
+
+    # create pool for storages
+    if not MANAGED_POOL:
+        return
+
+    xml_file = open("xml/pool.xml")
+    xml_pool = xml_file.read()
+    xml_pool = xml_pool.format(path=pool_path)
+    if not os.path.exists(pool_path):
+        os.mkdir(pool_path)
+    xml_dom = parseString(xml_pool)
+    pool_name = xml_dom.getElementsByTagName("name")
+    if len(pool_name) != 1:
+        inflogging.log("Bad pool XML.", "ERROR")
+        raise InfinityException("")
+    pool_name = pool_name[0].value
+    if pool_name not in LIBVIRT_CONNECTION.listAllStoragePools(0):
+        MANAGED_POOL = LIBVIRT_CONNECTION.storagePoolCreateXML(xml_pool, 0)
     else:
-        fullpath = os.path.join(config.RESOURCES_PATH, path)
-    path_dirname = os.path.dirname(fullpath)
+        raise InfinityException("Pool " + pool_name + " already exist, but it's not managed by infinity.")
 
-    return fullpath, path_dirname
+
+def clean_all():
+    """Clean all existing domains, storages and pool."""
+
+    global LIBVIRT_CONNECTION, MANAGED_POOL, MANAGED_VM_DOMAINS
+    if not LIBVIRT_CONNECTION:
+        return
+
+    for domain in MANAGED_VM_DOMAINS:
+        domain.destroy()
+
+    if MANAGED_POOL:
+        for volume in MANAGED_POOL.listAllVolumes(0):
+            volume.delete(0)
+
+    MANAGED_POOL.destroy()
+    MANAGED_POOL = None
+    MANAGED_VM_DOMAINS = []
 
 
 def create_vol(pool, number, path, capacity, path_to_xml="machines/storage.xml"):
@@ -143,25 +115,6 @@ def create_vol(pool, number, path, capacity, path_to_xml="machines/storage.xml")
 
     vol = pool.createXML(xmldesc, 0)
     return vol
-
-
-def create_pool(conn, path_to_xml="machines/pool.xml"):
-    """Create libvirt virtual pool.
-
-    Keyword arguments:
-    conn -- libvirt connection
-    path_to_xml -- path to configuration xml. See create_full_path function.
-
-    """
-    fullpath, path_dirname = create_full_path(path_to_xml)
-
-    with open(fullpath) as xmlfile:
-        xmldesc = xmlfile.read()
-
-    xmldesc = xmldesc.format(path_dirname)
-
-    pool = conn.storagePoolCreateXML(xmldesc, 0)
-    return pool
 
 
 def build(path_to_xml="machines/alfa.xml", ram=2097152):
